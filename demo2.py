9c7338cb32fa3d5e07d90c37c695ba661efb4d00
anagha-aravindh <anagha4113@gmail.com>
2024-05-10 13:21:09 +0530

    ran

---
+++ demo2.py
@@ -0,0 +1,17 @@
+x=10
+y=3.14
+z=5+4j
+print(x,y,z)
+print (type(x))
+print(type(y))
+print(type(z))
+a=float(x)
+print(a)
+print(type(a))
+b=int(y)
+print(b)
+print(type(b))
+c=complex(x)
+print(c)
+print(type(c))
+
