184744ef52b9243871f35fc617a9596e0af375a1
zenny <shilizenkv@gmail.com>
2024-05-10 11:53:57 +0530

    test commit

---
+++ add2no.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+print(a+b)
+a=10
+b=40
+print(a,b,a+b)
+a="i"
+b="am"
+print(a+" "+b)
+a=20
+b=10
+print(a-b)
+a=5
+b=2
+print(a/b)
+print(a*b)
---
+++ demo1.py
@@ -0,0 +1,18 @@
+print("good morning")
+print(1)
+#print(321)
+"""
+dfghgtdredrfhgbjyfrteerhjjugy
+bcfdvhjbjhghfv
+hgdg hgthjh
+"""
+'''jgufydy
+'''
+a=5
+print(a)
+x="apple"
+print(x)
+a,b,c="orange","kivi","apple"
+print(a,b,c)
+e=f=g="guva"
+print(e,f,g)
