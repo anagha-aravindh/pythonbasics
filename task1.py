0cf84f20f55face8a99b8ebc798d5f0ddcdecfae
anagha-aravindh <anagha4113@gmail.com>
2024-05-13 13:58:32 +0530

    msgg

---
+++ task1.py
@@ -0,0 +1,25 @@
+a="good"
+print(a)
+print(type(a))
+print(len(a))
+print(a[1])
+for i in a:
+    print(i)
+b="python,is intrept"
+print(b)
+print("is" in b)
+print("see" not in b)
+print(b[2:5])
+print(b[:7])
+print(b[11:])
+print(b[-1])
+print(b.upper())
+print(b.lower())
+print(b.strip())
+print(b.replace('p','h'))
+print(b.split(','))
+print(a+" "+b)
+age=50
+text="appu is {} years old old"
+print(text.format(age))
+print(text.count("old"))
