ada5c963c0eb927565ebf51d9d109f4444514de9
anagha-aravindh <169027160+anagha-aravindh@users.noreply.github.com>
2024-05-22 13:15:48 +0530

    Add files via upload

---
+++ conditionnn.py
@@ -0,0 +1,25 @@
+x=50
+y=50
+if x>y:
+    print("x is greater")
+else :
+    print("y is greater")
+a=100
+b=100
+if a>b:
+    print("a is greater")
+elif a==b:
+    print("a&b are equal")
+else:
+    print("b is greater")
+ab=15
+if ab>10:
+    print("ab is abow 10")
+    if ab>20:
+        print("ab is abow 20")
+    else:
+        print("ab is below 20")
+else:
+    print("ab is below 20")
+if x>y:
+    pass
---
+++ loopss.py
@@ -0,0 +1,16 @@
+# x=1
+# while x<=10:
+#     print(x)
+#     x+=1
+# y=1
+# while y<=10:
+#     print(y)
+#     if y==5:
+#         break
+#     y+=1
+a=0
+while a<=11:
+    a+=1
+    if a==5:
+        continue
+    print(a)
