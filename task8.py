4f4f7738345abe14b6a3048ac6211a66a523202d
anagha-aravindh <169027160+anagha-aravindh@users.noreply.github.com>
2024-05-31 13:47:05 +0530

    Add files via upload

--- task8.py
+++ task8.py
@@ -1,8 +1,9 @@
-# a=open("tsk1.txt","x")
-# a.close()
-# a=open("tsk1.txt","w")
-# a.write("good morning all")
-# a.close()
+import os
+a=open("tsk1.txt","x")
+a.close()
+a=open("tsk1.txt","w")
+a.write("good morning all ")
+a.close()
 a=open("tsk1.txt","r")
 print(a.read())
 print(a.read(5))
@@ -11,3 +12,26 @@ a.close()
 a=open("tsk1.txt","a")
 a.write("how are uh")
 a.close()
+b=open("tst2.txt","x")
+os.remove("tst2.txt")
+os.rmdir("sampllle")
+
+
+
+a=open("tsk1.txt","r")
+k=a.read()
+print(k)
+a.close()
+a=open("tsk3.txt","w")
+a.write(k)
+a.close()
+x=open("tsk3.txt","r")
+print(x.read())
+
+
+
+
+
+
+
+
